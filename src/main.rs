33def90ce6176e3600c365ba5bf0e46f610bb5f2
Tony Zhou <kotokz@chenpingdeiMac.local>
2015-12-23 00:16:39 +0800

    implements add_param

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,17 @@ use super_agent::SuperAgent;
 
 fn main() {
 
-    let agent = SuperAgent::new("http://xueqiu.com").unwrap();
-
-    // let r = agent.get("http://xueqiu.com/statuses/topic.json?simple_user=1&topicType=5&page=1")
-    //             .unwrap();
-
-    // println!("{}", r.body);
-
+    let mut agent = SuperAgent::new("http://xueqiu.com").unwrap();
     let r = agent.get_with_params("http://xueqiu.com/statuses/topic.json",
-                                  Some(&[("simple_user", "1"), ("topicType", "5"), ("page", "1")]))
+                                  &[("simple_user", "1"), ("topicType", "5"), ("page", "1")])
                  .unwrap();
 
     println!("{}", r.body);
+
+    let r = agent.add_param("simple_user", "1")
+                 .add_param("topicType", "5")
+                 .add_param("page", "1")
+                 .get("http://xueqiu.com/statuses/topic.json")
+                 .unwrap();
+    println!("{}", r.code);
 }
--- src/super_agent.rs
+++ src/super_agent.rs
@@ -1,8 +1,8 @@
 use hyper::Client;
+use hyper::client::IntoUrl;
 use hyper::header::{Cookie, SetCookie, Connection};
 use cookie::CookieJar;
 use std::io::Error as IoError;
-use url::Url;
 use url::ParseError;
 use hyper::Error as HttpError;
 use std::fmt::{Display, Formatter};
@@ -13,6 +13,7 @@ pub struct SuperAgent<'a> {
     login_url: &'a str,
     client: Client,
     cookiejar: CookieJar<'a>,
+    params: Option<Vec<(&'a str, &'a str)>>,
 }
 
 impl<'a> SuperAgent<'a> {
@@ -24,46 +25,19 @@ impl<'a> SuperAgent<'a> {
             login_url: url,
             client: client,
             cookiejar: c,
+            params: None,
         };
         agent.initial_cookie();
         Ok(agent)
     }
 
-    pub fn get(&self, url: &'a str) -> Result<Response, AgentError> {
-        let mut resp = try!(self.client
-                                .get(url)
-                                .header(Connection::close())
-                                .header(Cookie::from_cookie_jar(&self.cookiejar))
-                                .send()
-                                .map_err(AgentError::HttpRequestError));
-        let mut resp_body = String::new();
-
-        try!(resp.read_to_string(&mut resp_body).map_err(AgentError::HttpIoError));
-
-        let response = Response {
-            code: resp.status.to_u16(),
-            status: resp.status,
-            headers: resp.headers.clone(),
-            body: resp_body,
-        };
-
-        return Ok(response);
-    }
-
-    pub fn get_with_params(&self,
-                           url: &'a str,
-                           url_params: Option<&[(&str, &str)]>)
-                           -> Result<Response, AgentError> {
-        let mut link = try!(Url::parse(url).map_err(AgentError::UrlParseError));
-        match url_params {
-            Some(params) => {
-                link.set_query_from_pairs(params.to_vec().into_iter());
-            }
-            None => (),
-        };
+    pub fn get<U: IntoUrl>(&mut self, url: U) -> Result<Response, AgentError> {
+        let mut link = try!(url.into_url().map_err(AgentError::UrlParseError));
+        if let Some(ref params) = self.params {
+            link.set_query_from_pairs(params.to_vec().into_iter());
+        }
 
         println!("{:?}", link);
-
         let mut resp = try!(self.client
                                 .get(link)
                                 .header(Connection::close())
@@ -81,7 +55,32 @@ impl<'a> SuperAgent<'a> {
             body: resp_body,
         };
 
+        self.params = None;
+
         return Ok(response);
+    }
+
+    pub fn get_with_params(&mut self,
+                           url: &'a str,
+                           url_params: &[(&'a str, &'a str)])
+                           -> Result<Response, AgentError> {
+        self.params = Some(url_params.iter().cloned().collect());
+
+        return self.get(url);
+    }
+
+    pub fn add_param(mut self, key: &'a str, val: &'a str) -> SuperAgent<'a> {
+        {
+            let mut params = match self.params {
+                Some(ref mut p) => p,
+                None => {
+                    self.params = Some(Vec::new());
+                    self.params.as_mut().unwrap()
+                }
+            };
+            params.push((key, val));
+        }
+        self
     }
 
     pub fn initial_cookie(&mut self) {
@@ -116,4 +115,21 @@ pub enum AgentError {
     UrlParseError(ParseError),
     HttpRequestError(HttpError),
     HttpIoError(IoError),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn verify_get_with_params() {
+
+        let mut agent = SuperAgent::new("http://xueqiu.com").unwrap();
+
+        let r = agent.get("http://xueqiu.com/statuses/topic.json?simple_user=1&topicType=5&page=1")
+                     .unwrap();
+        let r2 = agent.get_with_params("http://xueqiu.com/statuses/topic.json",
+                                       &[("simple_user", "1"), ("topicType", "5"), ("page", "1")])
+                      .unwrap();
+        assert_eq!(r.body, r2.body);
+    }
 }
